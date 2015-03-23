398e1684afbd8ec73c4900772cb8081c776bd3e4
dmccarey <danmccarey@gmail.com>
2015-03-23 14:44:00 -0400

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Mi, $*/
+
+
+window.Mi = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Mi.init();
+});
