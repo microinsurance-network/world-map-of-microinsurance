afb668b61e0095851a02ca8a06f15f807fa4d19f
Drew Bollinger <drew@developmentseed.org>
2015-08-25 10:09:33 +0100

    Add base view used to create other views

--- app/scripts/views/country.js
+++ app/scripts/views/country.js
@@ -5,18 +5,10 @@ Mi.Views = Mi.Views || {};
 (function () {
   'use strict';
 
-  var blue = '#006Da1',
-      green = '#3a3',
-      red = 'maroon',
-      orange = '#a61',
-      purple = '#63b',
-      brown = '#321';
 
-  Mi.Views.Country = Backbone.View.extend({
+  Mi.Views.Country = Mi.Views.Base.extend({
 
   template: JST['app/scripts/templates/country.ejs'],
-  tagName: 'div',
-  el: '#content',
 
   initialize: function (options) {
     this.year = options.year;
@@ -80,7 +72,7 @@ Mi.Views = Mi.Views || {};
           value.mostRecent.value !== '' &&
           value.name != 'Life coverage ratio (excluding credit life)' &&
           value.name != 'Life and accident coverage ratio (excluding credit life)') {
-  
+
           var chartData = [];
           var yearLabels = [];
           $.each(value.timeseries, function(index, year) {
@@ -95,109 +87,7 @@ Mi.Views = Mi.Views || {};
       });
 
       $('.more-info').popover({placement: 'top', trigger: 'hover', viewport: '.row-country'});
-
-      // share links
-      $('#twitter-share-btn').attr('href', 'https://twitter.com/home?status=' + window.location.href);
-      $('#facebook-share-btn').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href);
-      $('#linkedin-share-btn').attr('href', 'https://www.linkedin.com/shareArticle?mini=true&url=' + window.location.href);
-    },
-
-
-    drawLineChart: function(id, data, categories, name, type) {
-
-      var color = this.getColor(2, type).hex();
-
-      $(id).highcharts({
-        chart: {
-      	  plotBorderColor: 'transparent',
-      	  backgroundColor: 'transparent',
-          style: {
-            fontFamily: "'Abel', sans-serif",
-          }
-      	},
-      	title: {
-      		text: '',
-      		x: -20 //center
-      	},
-      	subtitle: {
-      		text: '',
-      		x: -20
-      	},
-      	xAxis: {
-          categories: categories,
-          labels: {
-            enabled: true,
-            style: { color: '#aaa'}
-          }
-        },
-      	yAxis: {
-          allowDecimals: false,
-          min: 0,
-      		title: { text: '' },
-          labels: {
-            x: -8,
-            enabled: true,
-            style: { color: '#aaa'}
-          },
-          gridLineColor: '#fff',
-          tickWidth: 1,
-          tickLength: 5
-      	},
-      	credits: { enabled: false },
-      	tooltip: { valueSuffix: '%' },
-      	legend: {
-          layout: 'vertical',
-      		align: 'right',
-      		verticalAlign: 'middle',
-      		borderWidth: 0,
-      		enabled: false
-      	},
-      	series: [{
-      		name: 'Ratio',
-      		color: color,
-      		data: data
-      	}]
-      });
-    },
-
-    numberWithCommas: function(x) {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    },
-
-    capitalizeFirstLetter: function(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    },
-
-    typeToColor: function (type) {
-      switch (type) {
-        case 'total-microinsurance-coverage-ratio':
-          return blue;
-          break;
-        case 'credit-life-coverage-ratio':
-          return red;
-          break;
-        case 'health-coverage-ratio':
-          return green;
-          break;
-        case 'accident-coverage-ratio':
-          return orange;
-          break;
-        case 'property-coverage-ratio':
-          return brown;
-          break;
-        case 'agriculture-coverage-ratio':
-          return purple;
-          break;
-      }
-    },
-
-    getColor: function (value, type) {
-      var palette = ['white', this.typeToColor(type)];
-      var scale = chroma.scale(palette)
-        .mode('hsl')
-        .domain([-1, 0, 1, 2, 3, 4]);
-      return (value === 0) ? '#ddd' : scale(Math.floor(Math.min(value, 3)));
-    },
+    }
 
   });
 
--- app/scripts/views/global.js
+++ app/scripts/views/global.js
@@ -6,18 +6,9 @@ Mi.Views = Mi.Views || {};
 
 'use strict';
 
-var blue = '#006Da1',
-    green = '#3a3',
-    red = 'maroon',
-    orange = '#a61',
-    purple = '#63b',
-    brown = '#321';
-
-  Mi.Views.Global = Backbone.View.extend({
+  Mi.Views.Global = Mi.Views.Base.extend({
 
     template: JST['app/scripts/templates/global.ejs'],
-
-    el: '#content',
 
     initialize: function (options) {
       this.year = options.year;
@@ -38,7 +29,7 @@ var blue = '#006Da1',
 
       var _self = this;
 
-      $('.region-value').text(this.capitalizeFirstLetter(this.region));
+      $('.region-value').text(Mi.regions[this.region]);
       $('.type-value').text(this.capitalizeFirstLetter(this.type.replace(/-/g, ' ')));
       if (this.year === 'all') {
         $('.year-value').text('Most recent value');
@@ -48,11 +39,6 @@ var blue = '#006Da1',
 
       this.setView(this.region);
       this.drawLegend();
-
-      // share links
-      $('#twitter-share-btn').attr('href', 'https://twitter.com/home?status=' + window.location.href);
-      $('#facebook-share-btn').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href);
-      $('#linkedin-share-btn').attr('href', 'https://www.linkedin.com/shareArticle?mini=true&url=' + window.location.href);
 
       // render template
       this.$el.html(this.template({
@@ -71,100 +57,12 @@ var blue = '#006Da1',
       });
 
       _.each(this.data, function(value) {
-
-        Mi.countryGeo.eachLayer(function(layer){
-          if (value.iso === layer.feature.properties.iso_a3 && value.mainValue !== '') {
-            var color = _self.getColor(value.mainValue, _self.type);
-            layer.setStyle({
-              fillColor: color,
-              fillOpacity: (value.mainValue) ? 0.8 : 0.3
-            });
-            // make the countries with data clickable
-            if (layer._path) {
-              layer._path.setAttribute('class','leaflet-clickable');
-            } else {
-              _.each(layer._layers, function (l) {
-                l._path.setAttribute('class','leaflet-clickable');
-              });
-            }
-
-            var markup = '<div class="inner"><span class="country-name">' + value.country +
-            '</span><span class="year"> (' + value.filterYear + ')</span><br>' +
-            '<span class="popup-value">' + _self.capitalizeFirstLetter(_self.type.replace(/-/g, ' ')) + ': ' + value.mainValue + '%</span><br>' +
-                         '<a href="#country/' + value.iso +'">View profile</a></div>';
-            layer.bindPopup(markup, { autoPan: true });
-          }
-        });
-       });
-
-       $('#map').animate({height: '450px'});
-       $('.more-info').popover({placement: 'top', trigger: 'hover', viewport: '.row-country'});
-
-    },
-
-    capitalizeFirstLetter: function(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    },
-
-    drawLineChart: function(selector, data, categories, name, type, agg) {
-
-      var _self = this;
-      var typeUsed = type || this.type;
-      var color = _self.getColor(2, typeUsed).hex();
-
-      $(selector).highcharts({
-        chart: {
-          plotBorderColor: '#fff',
-          style: {
-            fontFamily: "'Abel', sans-serif",
-          }
-        },
-        title: { text: '', x: -20 /*center*/},
-        subtitle: { text: '', x: -20 },
-        xAxis: {
-          categories: categories,
-          labels: {
-            enabled: true,
-            style: { color: '#aaa'}
-          }
-        },
-        yAxis: {
-          allowDecimals: (!!agg),
-          min: 0,
-          title: { text: '' },
-          labels: {
-            x: -8,
-            enabled: true,
-            style: { color: '#aaa'}
-          },
-          gridLineColor: '#fff',
-          tickWidth: 1,
-          tickLength: 5
-        },
-        credits: { enabled: false },
-        tooltip: { valueSuffix: '%' },
-        legend: {
-          layout: 'vertical',
-          align: 'right',
-          verticalAlign: 'middle',
-          borderWidth: 0,
-          enabled: false
-        },
-        series: [{
-          color: color,
-          name: 'Ratio',
-          data: data
-        }]
+        _self.updateMap(value);
       });
 
-    },
+      $('#map').animate({height: '450px'});
+      $('.more-info').popover({placement: 'top', trigger: 'hover', viewport: '.row-country'});
 
-    numberWithCommas: function(x) {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    },
-
-    capitalizeFirstLetter: function(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
     },
 
     processData: function () {
@@ -305,132 +203,6 @@ var blue = '#006Da1',
       });
 
       this.regions = regions;
-    },
-
-    setView: function (region) {
-      switch (region) {
-        case 'africa':
-          Mi.map.setView([4.43, 28.83], 3);
-          break;
-        case 'americas':
-          Mi.map.setView([2.20, -77.26], 3);
-          break;
-        case 'asia':
-          Mi.map.setView([26.98, 87.10], 3);
-          break;
-        default:
-          Mi.map.setView([20, 0], 2);
-      }
-    },
-
-    drawLegend: function () {
-      var _self = this;
-
-      d3.select('.legend').select('*').remove();
-
-      var margin = {top: 20, right: 10, bottom: 10, left: 10};
-
-      var width = d3.select('.legend').style('width').replace('px','') - margin.left - margin.right,
-          height = d3.select('.legend').style('height').replace('px','') - margin.top - margin.bottom;
-
-      var svg = d3.select('.legend').append('svg')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-      var data = [0, 0.1, 1, 2, 3];
-
-      svg.selectAll('color')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('fill', function(d) { return _self.getColor(d, _self.type); })
-        .attr('opacity', function(d) { return d ? 0.8 : 0.4; })
-        .attr('width', width / data.length)
-        .attr('height', height)
-        .attr('y',0)
-        .attr('x', function(d,i) { return i * width / data.length});
-
-     svg.selectAll('label')
-       .data(data)
-       .enter()
-       .append('text')
-       .attr('class','label')
-       .attr('dy', -5)
-       .attr('x', function(d,i) { return (i + 0.5) * width / data.length; })
-       .attr('text-anchor','middle')
-       .text(function(d, i) {
-         if (d === 0) { return '0%'; }
-         return Math.floor(data[i]) +
-         ((data[i+1]) ? ' - ' + data[i+1] + '%' : '%+');
-       });
-    },
-
-    typeToColor: function (type) {
-      switch (type) {
-        case 'total-microinsurance-coverage-ratio':
-          return blue;
-          break;
-        case 'credit-life-coverage-ratio':
-          return red;
-          break;
-        case 'health-coverage-ratio':
-          return green;
-          break;
-        case 'accident-coverage-ratio':
-          return orange;
-          break;
-        case 'property-coverage-ratio':
-          return brown;
-          break;
-        case 'agriculture-coverage-ratio':
-          return purple;
-          break;
-      }
-    },
-
-    getColor: function (value, type) {
-      var palette = ['white', this.typeToColor(type)];
-      var scale = chroma.scale(palette)
-        .mode('hsl')
-        .domain([-1, 0, 1, 2, 3, 4]);
-      return (Number(value) === 0) ? '#ddd' : scale(Math.floor(Math.min(value, 3)));
-    },
-
-    resetMapStyle: function () {
-      Mi.countryGeo.eachLayer(function(layer){
-        layer.setStyle({
-          color: 'white',
-          weight: 1,
-          fillColor: 'url(#hash)',
-          fillOpacity: 1
-        });
-        // hack until https://github.com/Leaflet/Leaflet/issues/2662
-        // is resolved
-        d3.selectAll('.leaflet-overlay-pane path').classed('no-data', true);
-      });
-    },
-
-    regionMatch: function (region, regionGroup) {
-      switch (regionGroup) {
-        case 'africa':
-          return region === 'africa';
-          break;
-        case 'americas':
-          return region === 'americas';
-          break;
-        case 'asia':
-          return _.contains(['asia','oceania'], region);
-          break;
-      }
-    },
-
-    regionNav: function (e) {
-      e.preventDefault();
-      Mi.router.navigate('view/' + e.target.id.toLowerCase() + '/' + Mi.year +
-        '/' + Mi.name, {trigger: true});
-
     }
   });
 
