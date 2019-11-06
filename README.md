51b70aab7b842f3003a502e0a309271712ed90c8
Dan McCarey <danmccarey@gmail.com>
2019-11-05 19:20:20 -0500

    add new build

--- README.md
+++
@@ -1,8 +0,0 @@
-## World Map of Microinsurance
-
-This repository hosts the website for the [World Map of Microinsurance]().
-Update the content and data by following the instructions below. Update the
-code by checking out the `master` branch and following the build instructions
-located there.
-
-For steps to update the content and data, see the [Updating Content and Data wiki page](https://github.com/microinsurance-network/world-map-of-microinsurance/wiki/Updating-the-content-and-data).
---
+++ precache-manifest.41d026c5a9c6ee5a8e8dbc2e3a0c6ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d478e92b4df1ab5712cf52542ea8889a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db3ef26c79442b93dab",
+    "url": "/static/css/2.1a7ee29c.chunk.css"
+  },
+  {
+    "revision": "1628b721cbf3cccc241f",
+    "url": "/static/css/main.f6f82e75.chunk.css"
+  },
+  {
+    "revision": "0db3ef26c79442b93dab",
+    "url": "/static/js/2.6486ef38.chunk.js"
+  },
+  {
+    "revision": "1628b721cbf3cccc241f",
+    "url": "/static/js/main.b9de2ef0.chunk.js"
+  },
+  {
+    "revision": "cb568fd7f42adc50a533",
+    "url": "/static/js/runtime-main.2d7371b1.js"
+  }
+]);
